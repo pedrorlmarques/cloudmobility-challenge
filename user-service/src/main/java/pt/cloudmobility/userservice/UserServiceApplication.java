ca359f6fa9f7ba4f195fe7cf2734d386e31f45f7
Pedro Marques <pedrorlmarques@gmail.com>
2021-09-03 17:33:40 +0100

    feat(cloudmobility-challenge): adds skeleton project

---
+++ user-service/src/main/java/pt/cloudmobility/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package pt.cloudmobility.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/pt/cloudmobility/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.cloudmobility.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
