3dcb674514b1d9330fdb746c46c1acb205e321ed
Pedro Marques <pedrorlmarques@gmail.com>
2021-09-04 14:44:28 +0100

    ci(docker-compose): adds user-postgresql

---
+++ user-service/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "user"
+(
+    id                    SERIAL PRIMARY KEY,
+    first_name            varchar(50),
+    last_name             varchar(50),
+    identification_number varchar(50),
+    role                  varchar(50)
+);
+
