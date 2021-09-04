d75563ed43c906da9c54885469e8147f7cace507
Pedro Marques <pedrorlmarques@gmail.com>
2021-09-04 19:24:36 +0100

    build(pom.xml): adds idp-manager-service

---
+++ idp-manager-service/src/main/java/pt/cloudmobility/idpmanagerservice/IdpManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package pt.cloudmobility.idpmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdpManagerServiceApplication.class, args);
+	}
+
+}
