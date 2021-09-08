26ee37f1c26ea46ecc1c96f6bd79e90b87aac7ca
Pedro Marques <pedrorlmarques@gmail.com>
2021-09-08 12:13:07 +0100

    feat(appointment-service): adds logging on user consumer

--- appointment-service/src/main/java/pt/cloudmobility/appointmentservice/utils/UserEventFunctionHelper.java
+++ appointment-service/src/main/java/pt/cloudmobility/appointmentservice/utils/UserEventFunctionHelper.java
@@ -1,5 +1,7 @@
 package pt.cloudmobility.appointmentservice.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pt.cloudmobility.appointmentservice.dto.EventType;
 import pt.cloudmobility.appointmentservice.dto.InternalRole;
 import pt.cloudmobility.appointmentservice.dto.UserEvent;
@@ -12,6 +14,8 @@ import java.util.function.Predicate;
 
 public final class UserEventFunctionHelper {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserEventFunctionHelper.class);
+
     private UserEventFunctionHelper() {
         //private constructor
     }
@@ -20,6 +24,7 @@ public final class UserEventFunctionHelper {
         return userEventFlux -> userEventFlux
                 .filter(onlyUserCreationEvents())
                 .filter(onlyDoctorAllowed())
+                .doOnNext(userEvent -> logger.info("Received {}", userEvent))
                 .flatMap(userEvent -> scheduleService
                         .createDefaultWeekScheduleFor(userEvent.getSubject().getId()))
                 .then();
