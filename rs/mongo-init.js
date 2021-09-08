658607dcf97134514bb9cff1747f7a18afe9751c
Pedro Marques <pedrorlmarques@gmail.com>
2021-09-08 15:22:22 +0100

    ci(docker-compose): adds replica set mongodb and service images

---
+++ rs/mongo-init.js
@@ -0,0 +1,14 @@
+// initiate
+rs.initiate({
+    "_id":"appointmentrs",
+    "members":[
+        {
+            "_id":0,
+            "host":"mongo1:27017"
+        },
+        {
+            "_id":1,
+            "host":"mongo2:27017"
+        }
+    ]
+});
