e14501b020a73fb383932d4115e90c0a033380e7
Pedro Marques <pedrorlmarques@gmail.com>
2021-09-08 12:12:49 +0100

    feat(idp-manager-service): adds logging on user consumer

--- idp-manager-service/src/main/java/pt/cloudmobility/idpmanagerservice/utils/UserEventFunctionHelper.java
+++ idp-manager-service/src/main/java/pt/cloudmobility/idpmanagerservice/utils/UserEventFunctionHelper.java
@@ -1,5 +1,7 @@
 package pt.cloudmobility.idpmanagerservice.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pt.cloudmobility.idpmanagerservice.dto.UserEvent;
 import pt.cloudmobility.idpmanagerservice.factory.KeycloakServiceFactory;
 import pt.cloudmobility.idpmanagerservice.mapper.KeycloakUserInputRequestMapper;
@@ -8,13 +10,17 @@ import java.util.function.Consumer;
 
 public final class UserEventFunctionHelper {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserEventFunctionHelper.class);
+
     private UserEventFunctionHelper() {
         //private constructor
     }
 
     public static Consumer<UserEvent> onUserEvents(final KeycloakServiceFactory keycloakFactory) {
-        return userEvent ->
-                keycloakFactory.getInstance(userEvent.getEventType())
-                        .execute(KeycloakUserInputRequestMapper.INSTANCE.convertTo(userEvent));
+        return userEvent -> {
+            logger.info("Received {}", userEvent);
+            keycloakFactory.getInstance(userEvent.getEventType())
+                    .execute(KeycloakUserInputRequestMapper.INSTANCE.convertTo(userEvent));
+        };
     }
 }
